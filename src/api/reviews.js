08af866733cedfb9fb08d19a2b01d3cb0422010f
YarikTunev <165372196+YarikTunev@users.noreply.github.com>
2026-03-23 22:07:46 +0500

    Update reviews.js

--- src/api/reviews.js
+++ src/api/reviews.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.VUE_APP_API_URL || 'http://fooddelivery.s99220rx.beget.tech',
+  baseURL: process.env.VUE_APP_API_URL || 'http://fooddelivery.s99220rx.beget.tech/api',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
